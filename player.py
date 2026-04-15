21a13050d4adc8376ce126d593020d1efed876d5
mathis3733 <mathischou77@gmail.com>
2026-04-15 16:09:07 +0200

    Create player.py
    premier ajout

---
+++ player.py
@@ -0,0 +1,12 @@
+class player:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+    
+    def askmove(self):
+        while True:
+            move = input(f"{self.name} ({self.color}), enter your move (e.g. 'e2 e4'): ")
+            if self.validate_move(move):
+                return move
+            else:
+                print("Invalid move format. Please try again.")
